0e52485e1b267115e5e4b1818710053ccad657dd
Ian Field <ian.field90@gmail.com>
2015-03-17 23:35:35 +0000

    Remove redundant files

--- SE1SA5/T1P8A/dateUtil.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#define DATE_FIELD_SEPARATOR '/'
-int getDay(char date[]);
-int getMonth(char date[]);
-int getYear(char date[]);
-int dateCompare(char date1[], char date2[]);
--- Snip4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main ()
-{
-printf("sizeof short int %d\n", sizeof (short int));
-printf("sizeof float %d\n", sizeof (float));
-printf("sizeof double %d\n", sizeof (double));
-return 0;
-}
